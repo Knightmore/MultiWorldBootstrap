96fe40f2f03861b345b81b6dc56ef25c9fa50428
Knightmore <35805408+Knightmore@users.noreply.github.com>
2019-06-16 18:51:11 +0200

    Add files via upload

---
+++ CreateInWorldAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Apply to a system, system group or buffer system 
+/// to only create in specified world
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class CreateInWorldAttribute : Attribute
+{
+    public string Name;
+    public CreateInWorldAttribute(string name)
+    {
+        Name = name;
+    }
+}
